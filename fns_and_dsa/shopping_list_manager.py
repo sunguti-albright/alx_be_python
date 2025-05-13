7e798748cb4b94a8fb4adca26d1ffa0bc0f1099c
sunguti-albright <sungutialbright@gmail.com>
2025-05-13 16:40:54 +0300

    lists implementation in shopping lists

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,58 @@
+'''
+1. Shopping List Manager
+mandatory
+Objective: Utilize Python lists to create a simple shopping list manager that allows users to add items, view the current list, and remove items.
+
+Task Description:
+Create a Python script named shopping_list_manager.py that implements a simple interface for managing a shopping list. This task focuses on using lists to store and manipulate data dynamically.
+
+Requirements:
+Core Functionality:
+
+Your script should start with an empty list named shopping_list.
+Implement functionality to add items to the list, remove items, and display the current list.
+User Interface:
+
+Use a loop to continuously display a menu with options to the user until they choose to exit. The menu should offer options to add an item, remove an item, view the list, and exit.
+For adding items, prompt the user for the item name and append it to shopping_list.
+For removing items, ask the user for the item name and remove it from shopping_list. If the item is not found, display a message indicating so.
+To view the list, print each item in shopping_list to the console.
+Ensure your script handles invalid menu choices gracefully.
+'''
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            list_item = input("Add item: ")
+            shopping_list.append(list_item)
+            print(shopping_list)
+            pass
+        elif choice == '2':
+            item_to_remove = input("Which item would you want to remove from the list? ")
+            if item_to_remove in shopping_list:
+                shopping_list.remove(item_to_remove)
+                print(f"{item_to_remove} has been removed from the list")
+            else:
+                print("No such item exists in the list")
+            pass
+        elif choice == '3':
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
