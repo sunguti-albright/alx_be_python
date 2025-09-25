44820638559c57b8f22108a2480285686818cc75
sunguti-albright <sungutialbright@gmail.com>
2025-09-25 13:13:55 +0300

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,18 +10,31 @@ The function should execute the arithmetic operation based on the operation para
 For division, include handling for division by zero, returning a specific message or value that your main.py script can recognize and display appropriately.
 Return the result of the arithmetic operation.
 '''
+# def perform_operation(num1, num2, operation):
+   # match operation:
+   #    case "add":
+   #       return num1 + num2
+   #    case "subtract":
+   #       return num1 - num2
+   #    case "multiply":
+   #       return num1 * num2
+   #    case "divide":
+   #       if num2 == 0:
+   #          return("Cannot divide by zero")
+   #       else:
+   #          return num1 / num2
+      
 def perform_operation(num1, num2, operation):
-   match operation:
-      case "add":
-         return num1 + num2
-      case "subtract":
-         return num1 - num2
-      case "multiply":
-         return num1 * num2
-      case "divide":
-         if num2 == 0:
-            return("Cannot divide by zero")
-         else:
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
             return num1 / num2
-      
-      
+    else:
+        return "Invalid operation"    
